952e6b471eea04beb6af6739a2b3dfb008c200f6
bjsi <jamesbaghurst@gmail.com>
2020-02-23 12:53:41 +0000

    removed abstractmethod decorator

--- Queue/QueueBase.py
+++ Queue/QueueBase.py
@@ -1,9 +1,9 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import Dict, Callable
 
 
 class QueueBase(ABC):
-    """The abstract class all queues inherit from
+    """Base class all queues inherit from
     """
 
     def __init__(self):
@@ -17,14 +17,15 @@ class QueueBase(ABC):
 
         :clozing: True if currently clozing.
         """
+
+        super().__init__()
+
         self.current_queue: str = ""
         # Mapping of keycodes to queue methods.
         self.active_keys: Dict[int, Callable] = {}
         self.recording: bool = False
         self.clozing: bool = False
-        super().__init__()
 
-    @abstractmethod
     def load_initial_queue(self) -> bool:
         """Load the starting queue for this queue.
         :returns: True on success else fail.
