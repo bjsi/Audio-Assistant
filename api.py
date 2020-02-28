02c391f70d225f84d92e33e066d10bb452a07765
bjsi <jamesbaghurst@gmail.com>
2020-02-28 11:22:35 +0000

    small fix

--- api.py
+++ api.py
@@ -48,9 +48,10 @@ class Youtube(Resource):
     def post(self):
         """Start a youtube_dl download
         """
-        if download_request:
+        dl_req = request.get_json()
+        if dl_req:
             # TODO wait for the finished hook return status somehow
-            AudioDownloader(**download_request).download()
+            AudioDownloader(**dl_req).download()
             return download_request, 201
         return download_request, 404
 
